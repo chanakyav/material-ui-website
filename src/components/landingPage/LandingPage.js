8607d8c34d7ef6d4bceaed233d560dddb4bbecc4
Chanakya <chanakya.valluri@gmail.com>
2021-02-04 21:12:22 -0500

    Set up grid containers for landing page

--- src/components/landingPage/LandingPage.js
+++ src/components/landingPage/LandingPage.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Lottie from "react-lottie";
+import { Grid, Button } from "@material-ui/core";
 
+import { ButtonArrow } from "../ui";
 import animationData from "../../animations/landinganimation/data";
 import useStyles from "./landingPageStyles";
 
@@ -16,5 +18,32 @@ export function LandingPage() {
     },
   };
 
-  return <Lottie options={defaultOptions} height={"100%"} width={"100%"} />;
+  return (
+    <Grid container direction="column">
+      <Grid item>
+        <Grid container direction="row">
+          <Grid item>
+            <div>
+              Bringing West Coast Technology <br /> to the Midwest
+            </div>
+            <Grid container>
+              <Grid item>
+                <Button variant="contained">Free Estimate</Button>
+              </Grid>
+              <Grid item>
+                <Button variant="outlined">
+                  Learn More
+                  <ButtonArrow width={15} height={15} fill="red" />
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+
+          <Grid item>
+            <Lottie options={defaultOptions} height={"100%"} width={"100%"} />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 }
